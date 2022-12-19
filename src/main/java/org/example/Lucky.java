4cfde0c18aa9b7c97bede26db43a2acca254e8c5
Mikhail Egorov <m-5321e@yandex.com>
2022-12-19 09:04:16 +0500

    Егоров Михаил. task01

--- src/main/java/org/example/Lucky.java
+++ src/main/java/org/example/Lucky.java
@@ -3,23 +3,23 @@ package org.example;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Lucky {
-    static final AtomicInteger atomicX = new AtomicInteger(0);
+    static final AtomicInteger atomicX = new AtomicInteger(-1);
     static final AtomicInteger count = new AtomicInteger(0);
 
     static class LuckyThread extends Thread {
         @Override
         public void run() {
-            while (atomicX.get() < 999999) {
-                final int x;
-                synchronized (new Object()) {
-                    x = atomicX.incrementAndGet();
-                }
+            int x = atomicX.incrementAndGet();
+            while (x < 999999) {
+
                 if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
                         % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
                     System.out.println(Thread.currentThread().getName() + ": " + x);
 
                     count.incrementAndGet();
                 }
+
+                x = atomicX.incrementAndGet();
             }
         }
     }
